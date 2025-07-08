6301de15a2b4d56898c39cbca717152fbc2dcdee
MORDOHAI Florian <florian.mordohai@etu.univ-amu.fr>
2025-07-08 08:57:31 +0200

    Mise en place base projet

---
+++ client.py
@@ -0,0 +1,13 @@
+import requests
+
+def main():
+    try:
+        hello_resp = requests.get("http://localhost:8081", headers={"Host": "service_tests.localhost"})
+
+        print("Réponse du service Hello :", hello_resp.text)
+
+    except requests.exceptions.RequestException as e:
+        print("Erreur lors de l'appel aux services :", e)
+
+if __name__ == "__main__":
+    main()
